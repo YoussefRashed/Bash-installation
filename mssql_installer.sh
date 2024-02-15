a1c5a5b93ad48934a11bd8c26b551d39e44ef3a6
YoussefRashed <YousefRashed.Business@gmail.com>
2024-02-15 20:07:19 +0000

    ubdate libssl1.1_1.1.1f path

--- mssql_installer.sh
+++ mssql_installer.sh
@@ -14,9 +14,9 @@ sudo apt-get install -y mssql-server
 echo '#########################===> end of mssql installation'
 echo '#########################===> start Adding missed Libs of mssql'
 
-wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
+wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.21_amd64.deb
 
-sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
+sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.21_amd64.deb
 
 sudo wget http://http.us.debian.org/debian/pool/main/o/openldap/libldap-2.4-2_2.4.47+dfsg-3+deb10u7_amd64.deb
 
